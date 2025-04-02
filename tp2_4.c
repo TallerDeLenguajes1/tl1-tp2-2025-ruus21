bbb892ede2e668429acd4e1bcc0dbc06b4c9dffe
Ruth <easerratic21>
2025-04-02 19:18:16 -0300

    generacion de datos aleatorios y almacenamiento de datos

--- tp2_4.c
+++ tp2_4.c
@@ -10,3 +10,23 @@ struct compu {
     int cantidad_nucleos; // Cantidad de núcleos (1-8)
     char *tipo_cpu; // Tipo de procesador
 };
+char *tipos[6] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+void generarPCs(struct compu pcs[], int cantidad);
+int main() {
+    srand(time(NULL));
+    struct compu pcs[CANT_PC];
+    
+    generarPCs(pcs, CANT_PC);
+    
+    return 0;
+}
+
+void generarPCs(struct compu pcs[], int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
+        pcs[i].velocidad = rand() % 3 + 1; // 1 a 3 GHz
+        pcs[i].anio = rand() % 10 + 2015; // 2015 a 2024
+        pcs[i].cantidad_nucleos = rand() % 8 + 1; // 1 a 8 núcleos
+        pcs[i].tipo_cpu = tipos[rand() % 6];
+    }
+}
