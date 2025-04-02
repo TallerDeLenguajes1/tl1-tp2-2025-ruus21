f728f9125ee5e73a830c6fcf36f8bfad3a786e6c
Ruth <easerratic21>
2025-04-02 19:19:41 -0300

    listar pcs

--- tp2_4.c
+++ tp2_4.c
@@ -13,12 +13,15 @@ struct compu {
 char *tipos[6] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
 void generarPCs(struct compu pcs[], int cantidad);
+void listarPCs(struct compu pcs[], int cantidad);
+
 int main() {
     srand(time(NULL));
     struct compu pcs[CANT_PC];
     
     generarPCs(pcs, CANT_PC);
-    
+    listarPCs(pcs, CANT_PC);
+
     return 0;
 }
 
@@ -28,5 +31,13 @@ void generarPCs(struct compu pcs[], int cantidad) {
         pcs[i].anio = rand() % 10 + 2015; // 2015 a 2024
         pcs[i].cantidad_nucleos = rand() % 8 + 1; // 1 a 8 núcleos
         pcs[i].tipo_cpu = tipos[rand() % 6];
+    }
+}
+
+void listarPCs(struct compu pcs[], int cantidad) {
+    printf("\nLista de PCs:\n");
+    for (int i = 0; i < cantidad; i++) {
+        printf("PC: Velocidad: %d GHz, Año: %d, Núcleos: %d, Tipo CPU: %s\n",
+               pcs[i].velocidad, pcs[i].anio, pcs[i].cantidad_nucleos, pcs[i].tipo_cpu);
     }
 }
